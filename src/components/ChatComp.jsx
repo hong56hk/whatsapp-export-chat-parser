40cd8d1fa4e082445c2c40711bea502b9dc82a14
hong <mcsquare@gmail.com>
2022-11-03 10:26:17 +0000

    init

--- src/components/ChatComp.jsx
+++ src/components/ChatComp.jsx
@@ -1,53 +1,46 @@
 import { useState } from 'react'
 
 const ChatComp = (props) => {
-  const [uiFolderPath, setUiFolderPath] = useState('');
-  const [uittt, setuittt] = useState('');
+  const [uiTitle, setUiTitle] = useState('Chat history');
+  const [msgArray, setMsgArray] = useState([]); // array of {time, sender, msg}
   
-  const onFolderPathChanged = (event) => {
-    //if we didnd't already have the "fileInput" var in scope, we could use "event.target" to get it
-    // if(fileInput.files.length>=1) {
-    //   //In this example, I'm putting the selected file's name in the title. You don't need to do this
-    //   document.title = fileInput.files[0].name;
-    //   downloadAnchor.setAttribute("download","edited_"+fileInput.files[0].name);
-    // }
-    // else {
-    //     document.title = "FileReader Example";
-    //     downloadAnchor.setAttribute("download","edited_file.txt");
-    // }
-    setUiFolderPath(event.target.value);
-    var fr = new FileReader();
-    fr.readAsText(uiFolderPath);
-    fr.onload = function (event) {
-        setuittt(event.target.result);
-        
+
+  const parseChat = (chat) => {
+    const pattern = /^\[([1-3]?[0-9]\/1?[0-9]\/[0-9]{4}, 1?[0-9]:[1-6]?[0-9]:[1-6]?[0-9] [AP]M)\] ([\w ]+): ([\w\W]+)/;
+    
+    const msgarr = [];
+    const lines = chat.split("\r\n");
+    for( const line of lines) {
+      const match = line.match(pattern);
+      msgarr.push({
+        time: match[1],
+        sender: match[2],
+        msg: match[3],
+      });
     }
+
+    console.log(msgarr);
+    return msgarr;
   }
 
   const onOpenChatClicked = async (event) => {
     const folderHandler = await window.showDirectoryPicker();
-    // for await (const entry of dirHandle.values()) {
-    //   if (entry.kind === "file"){
-    //     const file = await entry.getFile();
-    //     const text = await file.text();
-    //     console.log(text);
-    //   }
-    //   if (entry.kind === "directory"){
-    //     /* for file in this directory do something */ 
-    //   }
-    // }
+    setUiTitle(folderHandler.name + " Chat History");
+    
+    const fileHandle = await folderHandler.getFileHandle("_chat.txt");
+    const txtFile = await fileHandle.getFile(); // API: https://developer.mozilla.org/en-US/docs/Web/API/File
+    const chat = await txtFile.text();
+
+    setMsgArray(parseChat(chat));
+    
   }
 
   return (
     <div>
-      <h1>Chat history</h1>
-      <button onClick={onOpenChatClicked}>Open Chat</button>
-      <input id="fileInput" 
-        type="file" 
-        value={uiFolderPath}
-        onChange={onFolderPathChanged}
-      />
-      <textarea value={uittt}></textarea>
+      <h1>{uiTitle}</h1>
+      <button onClick={onOpenChatClicked}>Select Chat History Folder</button>
+      
+      
     </div>
   )
 }
